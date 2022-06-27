3ddca5e47bb2bf2505ba3fcd91a59423e492d406
JPeterD <jahrespeter@gmail.com>
2022-06-27 17:22:50 -0400

    Add Business Client Page

---
+++ src/pages/BusinessLanding/BusinessLanding.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import styles from "./styles/Business";
+
+const BusinessLanding = () => {
+	return (
+            <Box sx={{ ...styles.root }}>
+                <Typography>Business Landing Page</Typography>
+            </Box>
+    );
+}
+
+export default BusinessLanding;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import Home from "./pages/Home/Home.jsx";
+import BusinessLanding from "./pages/BusinessLanding/BusinessLanding.jsx";
 
 const routes = [
 	{
 		id: 0,
 		path: "/",
 		element: <Home />,
+		exact: true,
+	},
+
+	{
+		id: 2,
+		path: "/business",
+		element: <BusinessLanding/>,
 		exact: true,
 	},
 ];
